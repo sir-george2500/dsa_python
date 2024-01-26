2aeed7c12bcfecca018d3618447162c5f6b4a916
George S. Mulbah <gsmulbah2500@gmail.com>
2024-01-26 12:19:19 +0000

    added selection sort

---
+++ sorting_aglorithms/selection_sort.py
@@ -0,0 +1,17 @@
+
+def selection_sort(arr):
+    n = len(arr)
+
+    for i in range(n -1):
+        min_index = i
+
+        for j in range(i+1,n):
+
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+
+
+print(selection_sort([4,3,5,2,1]));
